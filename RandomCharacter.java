ead80d0af85cd4299d9070f9d82d6573ff103953
Ryan Sim <59084058+RyanSimYH@users.noreply.github.com>
2022-03-29 11:55:34 +0800

    Initial upload of Q1 and 2 of Lab 9

---
+++ RandomCharacter.java
@@ -0,0 +1,62 @@
+package Lab9;
+
+import java.util.Random;
+
+public class RandomCharacter
+{
+    public RandomCharacter()
+    {}
+    private int RandGen(int mode)
+    {
+        Random randGen = new Random();
+        switch (mode)
+        {
+            case 0:
+                return randGen.nextInt(97,122);
+            case 1:
+                return randGen.nextInt(48,57);
+
+            case 2:
+                return randGen.nextInt(33,126);
+
+            default:
+                return randGen.nextInt(65,90);
+        }
+
+    }
+    public char getRandomLowerCaseLetter()
+    {
+        return (char)this.RandGen(0);
+    }
+    public char getRandomUpperCaseLetter()
+    {
+        return (char)this.RandGen(3);
+
+    }
+    public char getRandomDigitCharacter()
+    {
+        return (char)this.RandGen(1);
+    }
+    public char getRandomCharacter()
+    {
+        return (char)this.RandGen(2);
+    }
+
+    public static void main(String[] args)
+    {
+        int j =0;
+        RandomCharacter i = new RandomCharacter();
+        System.out.println();
+        while (j<15)
+        {
+            System.out.println("LowerCase: "+i.getRandomLowerCaseLetter());
+            System.out.println("UpperCase: "+i.getRandomUpperCaseLetter());
+            System.out.println("Digit: "+i.getRandomDigitCharacter());
+            System.out.println("Random Char: "+i.getRandomCharacter()+"\n");
+            System.out.println("Prime Number Check:"+i.getRandomDigitCharacter());
+            j++;
+        }
+
+    }
+
+}
